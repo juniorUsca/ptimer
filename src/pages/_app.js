6d0f9827b751d10f5decd722f9b0eecf48cbeade
juniorUsca <junior.usca@gmail.com>
2020-11-03 22:33:24 -0500

    Config tailwind

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/tailwind.css'
+
+/* eslint-disable */
+function MyApp ({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+/* eslint-enable */
+
+export default MyApp
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => (
+  <>
+    <div className="justify-center p-24 bg-white rounded-lg">
+      <h1 className="text-lg">Junior Usca dice hola!!</h1>
+    </div>
+  </>
+)
+
+export default Home
